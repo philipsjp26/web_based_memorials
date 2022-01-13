d24e022c42d10757d70bb250fe2c350935cf3785
philipsjp26 <philipsjp12@gmail.com>
2022-01-14 02:45:24 +0700

    updated detail pages

--- resources/views/pages/memorials/details.blade.php
+++ resources/views/pages/memorials/details.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.home')
 @section('content')
+
     <!-- page main wrapper start -->
     <div class="shop-main-wrapper section-space">
         <div class="container">
@@ -35,145 +36,72 @@
                                         eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
                                         voluptua. Phasellus id nisi quis justo tempus mollis sed et dui. In hac
                                         habitasse platea dictumst.</p>
-
+                                    <button class="btn btn__bg" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                        Edit
+                                    </button>
+                                    <button class="btn btn__primary">
+                                        Add Images
+                                    </button>
                                 </div>
                             </div>
+
                         </div>
                     </div>
                     <!-- product details inner end -->
-
-                    <!-- product details reviews start -->
-                    <div class="product-details-reviews section-space pb-0">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="product-review-info">
-                                    <ul class="nav review-tab">
-                                        <li>
-                                            <a class="active" data-bs-toggle="tab" href="#tab_one">description</a>
-                                        </li>
-                                        <li>
-                                            <a data-bs-toggle="tab" href="#tab_two">information</a>
-                                        </li>
-                                        <li>
-                                            <a data-bs-toggle="tab" href="#tab_three">reviews (1)</a>
-                                        </li>
-                                    </ul>
-                                    <div class="tab-content reviews-tab">
-                                        <div class="tab-pane fade show active" id="tab_one">
-                                            <div class="tab-one">
-                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-                                                    fringilla augue nec est tristique auctor. Ipsum metus feugiat
-                                                    sem, quis fermentum turpis eros eget velit. Donec ac tempus
-                                                    ante. Fusce ultricies massa massa. Fusce aliquam, purus eget
-                                                    sagittis vulputate, sapien libero hendrerit est, sed commodo
-                                                    augue nisi non neque.Cras neque metus, consequat et blandit et,
-                                                    luctus a nunc. Etiam gravida vehicula tellus, in imperdiet
-                                                    ligula euismod eget. Pellentesque habitant morbi tristique
-                                                    senectus et netus et malesuada fames ac turpis egestas. Nam
-                                                    erat mi, rutrum at sollicitudin rhoncus</p>
+                    <!-- Quick view modal start -->
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+                        aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" style="max-width: 500px" role="document">
+                            <div class="modal-content">
+                                <div style="padding: 1rem 1rem; display: flex; align-items: center;" class="modal-header">
+                                    <h5 style="font-size: 1.25rem" class="modal-title" id="exampleModalLabel">Edit
+                                        Memorials
+                                    </h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form>
+                                        <div class="form-group col-md-12">
+                                            <label for="exampleInputEmail1">First name</label>
+                                            <input type="type" class="form-control" placeholder="Enter first name">
+                                        </div>
+                                        <div class="form-group col-md-12">
+                                            <label for="exampleInputEmail1">Middle name</label>
+                                            <input type="type" class="form-control" placeholder="Enter middle name">
+                                        </div>
+                                        <div class="form-group col-md-12">
+                                            <label for="exampleInputEmail1">Last name</label>
+                                            <input type="type" class="form-control" placeholder="Enter last name">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">Gender : </label>
+                                            <div class="form-check">
+                                                <input name="gender" value="Male" class="form-check-input" type="radio"
+                                                    name="flexRadioDefault" id="flexRadioDefault1">
+                                                <label class="form-check-label" for="flexRadioDefault1">
+                                                    Male
+                                                </label>
+                                            </div>
+                                            <div class="form-check">
+                                                <input name="gender" value="Female" class="form-check-input" type="radio"
+                                                    name="flexRadioDefault" id="flexRadioDefault2" checked>
+                                                <label class="form-check-label" for="flexRadioDefault2">
+                                                    Female
+                                                </label>
                                             </div>
                                         </div>
-                                        <div class="tab-pane fade" id="tab_two">
-                                            <table class="table table-bordered">
-                                                <tbody>
-                                                    <tr>
-                                                        <td>First Name</td>
-                                                        <td>Middle Name</td>
-                                                        <td>Last Name</td>
-                                                        <td>Gender</td>
-                                                    </tr>
-                                                    <tr>
-
-                                                        <td>{{ $data->first_name }}</td>
-                                                        <td>{{ $data->middle_name }}</td>
-                                                        <td>{{ $data->last_name }}</td>
-                                                        <td>{{ $data->gender }}</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                        <div class="tab-pane fade" id="tab_three">
-                                            <form action="#" class="review-form">
-                                                <h5>1 review for <span>Chaz Kangeroo</span></h5>
-                                                <div class="total-reviews">
-                                                    <div class="rev-avatar">
-                                                        <img src="assets/img/about/avatar.jpg" alt="">
-                                                    </div>
-                                                    <div class="review-box">
-                                                        <div class="ratings">
-                                                            <span class="good"><i
-                                                                    class="fa fa-star"></i></span>
-                                                            <span class="good"><i
-                                                                    class="fa fa-star"></i></span>
-                                                            <span class="good"><i
-                                                                    class="fa fa-star"></i></span>
-                                                            <span class="good"><i
-                                                                    class="fa fa-star"></i></span>
-                                                            <span><i class="fa fa-star"></i></span>
-                                                        </div>
-                                                        <div class="post-author">
-                                                            <p><span>admin -</span> 30 Mar, 2019</p>
-                                                        </div>
-                                                        <p>Aliquam fringilla euismod risus ac bibendum. Sed sit
-                                                            amet sem varius ante feugiat lacinia. Nunc ipsum nulla,
-                                                            vulputate ut venenatis vitae, malesuada ut mi. Quisque
-                                                            iaculis, dui congue placerat pretium, augue erat
-                                                            accumsan lacus</p>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group row">
-                                                    <div class="col">
-                                                        <label class="col-form-label"><span class="text-danger">*</span>
-                                                            Your Name</label>
-                                                        <input type="text" class="form-control" required>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group row">
-                                                    <div class="col">
-                                                        <label class="col-form-label"><span class="text-danger">*</span>
-                                                            Your Email</label>
-                                                        <input type="email" class="form-control" required>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group row">
-                                                    <div class="col">
-                                                        <label class="col-form-label"><span class="text-danger">*</span>
-                                                            Your Review</label>
-                                                        <textarea class="form-control" required></textarea>
-                                                        <div class="help-block pt-10"><span
-                                                                class="text-danger">Note:</span>
-                                                            HTML is not translated!
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group row">
-                                                    <div class="col">
-                                                        <label class="col-form-label"><span class="text-danger">*</span>
-                                                            Rating</label>
-                                                        &nbsp;&nbsp;&nbsp; Bad&nbsp;
-                                                        <input type="radio" value="1" name="rating">
-                                                        &nbsp;
-                                                        <input type="radio" value="2" name="rating">
-                                                        &nbsp;
-                                                        <input type="radio" value="3" name="rating">
-                                                        &nbsp;
-                                                        <input type="radio" value="4" name="rating">
-                                                        &nbsp;
-                                                        <input type="radio" value="5" name="rating" checked>
-                                                        &nbsp;Good
-                                                    </div>
-                                                </div>
-                                                <div class="buttons">
-                                                    <button class="sqr-btn" type="submit">Continue</button>
-                                                </div>
-                                            </form> <!-- end of review-form -->
-                                        </div>
-                                    </div>
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn__primary" data-dismiss="modal">Save
+                                        Changes</button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <!-- product details reviews end -->
+                    <!-- Quick view modal end -->
                 </div>
                 <!-- product details wrapper end -->
             </div>
