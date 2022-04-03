b39e2372f9e39e2a093e9d08041047edf02f7544
Philips Jose Patric <66919081+philipsjp26@users.noreply.github.com>
2022-04-03 21:27:53 +0700

    Fixing 3 April & resolve conflicts (#6)
    * handling register already exist
    
    * fix update data memorials
    
    * handling validate more upload image
    
    * fixing 03 April
    
    * remove middleware
    
    * fixing 3 April

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,35 +1,37 @@
 @extends('layouts.auth')
 
 @section('content')
-    <div class="login-register-wrapper section-space">
-        <div class="container" style="width: 25%">
-            <div class="member-area-from-wrap">
-                <div class="row">
-                    <!-- Login Content Start -->
-                    <div class="login-reg-form-wrap">
-                        <h2>Sign In</h2>
-                        <form action="/auth/login" method="post">
-                            @csrf
-                            <div class="single-input-item">
-                                <input class="form-control" type="email" name="email" placeholder="Email or Username"
-                                    @error('email') is-invalid @enderror autofocus required autocomplete="off"
-                                    value="{{ old('email') }}" />
-                            </div>
-                            <div class="single-input-item">
-                                <input type="password" name="password" placeholder="Enter your Password" required />
-                            </div>
-                            <div class="single-input-item">
-                                <div class="login-reg-form-meta d-flex align-items-center justify-content-between">
-                                    <a href="/auth/register" class="forget-pwd">Sign up</a>
-                                    <a href="/auth/forgotPassword" class="forget-pwd">Forget Password?</a>
+    <div class=" bg-img" data-bg={{ asset('img/background/main_background.webp') }}>
+        <div class="login-register-wrapper section-space">
+            <div class="container" style="width: 25%">
+                <div class="member-area-from-wrap">
+                    <div class="row">
+                        <!-- Login Content Start -->
+                        <div class="login-reg-form-wrap">
+                            <h2>Sign In</h2>
+                            <form action="/auth/login" method="post">
+                                @csrf
+                                <div class="single-input-item">
+                                    <input class="form-control" type="email" name="email" placeholder="Email or Username"
+                                        @error('email') is-invalid @enderror autofocus required autocomplete="off"
+                                        value="{{ old('email') }}" />
                                 </div>
-                            </div>
-                            <div class="single-input-item">
-                                <button class="btn btn__bg">Login</button>
-                            </div>
-                        </form>
+                                <div class="single-input-item">
+                                    <input type="password" name="password" placeholder="Enter your Password" required />
+                                </div>
+                                <div class="single-input-item">
+                                    <div class="login-reg-form-meta d-flex align-items-center justify-content-between">
+                                        <a href="/auth/register" class="forget-pwd">Sign up</a>
+                                        <a href="/auth/forgotPassword" class="forget-pwd">Forget Password?</a>
+                                    </div>
+                                </div>
+                                <div class="single-input-item">
+                                    <button class="btn btn__bg">Login</button>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- Login Content End -->
                     </div>
-                    <!-- Login Content End -->
                 </div>
             </div>
         </div>
